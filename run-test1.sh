37d87c746a43654e8ed1aa7e167e49dcbfdf2044
Nikhil Kini <nkini@ucsc.edu>
2017-09-26 16:53:20 -0700

    Configured as mentioned in the Loony Corn Udemy course. Doesn't work.

---
+++ run-test1.sh
@@ -0,0 +1,1 @@
+hadoop jar $HADOOP_HOME/share/hadoop/yarn/hadoop-yarn-applications-distributedshell-2.8.0.jar org.apache.hadoop.yarn.applications.distributedshell.Client --jar $HADOOP_HOME/share/hadoop/yarn/hadoop-yarn-applications-distributedshell-2.8.0.jar --shell_command date 2>&1 | tee test1
