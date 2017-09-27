19dd690383b0894e5b43e9d1f3ca819e758867d5
Nikhil Kini <nkini@ucsc.edu>
2017-09-26 17:22:04 -0700

    Something changed, but it's not yet successfully running. source: https://stackoverflow.com/questions/20586920

--- run-test1.sh
+++ run-test1.sh
@@ -1,1 +1,2 @@
-hadoop jar $HADOOP_HOME/share/hadoop/yarn/hadoop-yarn-applications-distributedshell-2.8.0.jar org.apache.hadoop.yarn.applications.distributedshell.Client --jar $HADOOP_HOME/share/hadoop/yarn/hadoop-yarn-applications-distributedshell-2.8.0.jar --shell_command date 2>&1 | tee test1
+#!/bin/bash
+hadoop jar /opt/hadoop-2.8.0/share/hadoop/yarn/hadoop-yarn-applications-distributedshell-2.8.0.jar org.apache.hadoop.yarn.applications.distributedshell.Client --jar /opt/hadoop-2.8.0/share/hadoop/yarn/hadoop-yarn-applications-distributedshell-2.8.0.jar --shell_command date 2>&1 | tee test1
